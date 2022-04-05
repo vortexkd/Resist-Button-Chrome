ca657ee8c933a19418da67c3c8b782105d656723
Sharat Chinnapa <vortexkd@gmail.com>
2022-04-05 17:08:03 +0900

    added basic tests for common.js

---
+++ test/common.test.js
@@ -0,0 +1,37 @@
+import {assert} from 'chai';
+import { insertResistButton } from '../src/content/common.js';
+import { JSDOM } from "jsdom";
+
+
+describe('common', function() {
+  describe('insertResistButton', function() {
+    it ('properly inserts a button given a button id.', () => {
+      const dom = new JSDOM(`<!DOCTYPE html><p><button id="buyButtonId">Buy Button </button></p>`);
+      const docRef = global.document;
+      global.document = dom.window.document;
+
+      const res = insertResistButton(
+        'buyButtonId',
+        'resistButtonId',
+        'Resist!!'
+      );
+      assert.equal(res.innerText, 'Resist!!');
+      assert.equal(res.id, 'resistButtonId');
+      assert.isNotNull(dom.window.document.getElementById('resistButtonId'));
+      global.document = docRef;
+    });
+    it('does not do anything if buyButtonId is not on the page.', () => {
+      const dom = new JSDOM(`<!DOCTYPE html><p></p>`);
+      const docRef = global.document;
+      global.document = dom.window.document;
+
+      const res = insertResistButton(
+        'buyButtonIdNotExist',
+        'resistButtonIdNotExist',
+        'Resist!!'
+      );
+      assert.isNull(dom.window.document.getElementById('resistButtonIdNotExist'));
+      global.document = docRef;
+    });
+  });
+})
