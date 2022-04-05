db8feedc1aa42ddc4433d54b5bf6dbe68df461f3
Sharat Chinnapa <vortexkd@gmail.com>
2022-04-05 16:27:32 +0900

    added confirmation page resist button for amazon.com

--- src/content/amazon_com_scripts.js
+++ src/content/amazon_com_scripts.js
@@ -4,3 +4,7 @@ import { insertResistButton } from "./common.js";
 insertResistButton('exports_desktop_qualifiedBuybox_buyNow_feature_div',
   'amazon_resist_button',
   'Resist!!');
+
+insertResistButton('placeYourOrder',
+  'amazon_resist_button',
+  'Resist!!');
