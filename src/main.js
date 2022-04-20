0d3ae9d873da1b18a826bea7ced21abfb42dcad6
Atharva Hudlikar <atharvahudlikar@gmail.com>
2022-04-20 22:34:24 +0530

    added amazon.in support as well as one bug fix in amazon.co.jp

--- src/main.js
+++ src/main.js
@@ -8,9 +8,11 @@ function pickCorrectJs() {
     return 'src/content/amazon_com_scripts.js';
   } else if (host.match('amazon.co.jp')) {
     return 'src/content/amazon_co_jp_scripts.js';
+  } else if (host.match('amazon.in')) {
+    return 'src/content/amazon_in_scripts.js';
   } else if (host.match('rakuten.co.jp')) {
     return 'src/content/rakuten_co_jp_scripts.js';
-  }
+  } 
   return null;
 }
 function insertScriptTag(url) {
